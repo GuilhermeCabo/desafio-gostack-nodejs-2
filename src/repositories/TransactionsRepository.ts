023b3d4f456779638b8ef1008fae3e44357726c6
Guilherme Cabo <guilherme.pcabo@gmail.com>
2020-04-15 21:15:18 -0300

    Desafio GoStack - Node.js (2) Finalizado

--- src/repositories/TransactionsRepository.ts
+++ src/repositories/TransactionsRepository.ts
@@ -6,6 +6,12 @@ interface Balance {
   total: number;
 }
 
+interface CreateTransaction {
+  title: string;
+  value: number;
+  type: 'income' | 'outcome';
+}
+
 class TransactionsRepository {
   private transactions: Transaction[];
 
@@ -14,15 +20,45 @@ class TransactionsRepository {
   }
 
   public all(): Transaction[] {
-    // TODO
+    return this.transactions;
   }
 
   public getBalance(): Balance {
-    // TODO
+    const balance = {
+      income: 0,
+      outcome: 0,
+      total: 0,
+    };
+
+    this.transactions.forEach(({ value, type }) => {
+      switch (type) {
+        case 'income':
+          balance.income += value;
+          balance.total += value;
+          break;
+
+        case 'outcome':
+          balance.outcome += value;
+          balance.total -= value;
+          break;
+
+        default:
+      }
+    });
+
+    return balance;
   }
 
-  public create(): Transaction {
-    // TODO
+  public create({ title, value, type }: CreateTransaction): Transaction {
+    const transaction = new Transaction({
+      title,
+      value,
+      type,
+    });
+
+    this.transactions.push(transaction);
+
+    return transaction;
   }
 }
 
